30cfda5a6904d4c2eef5de58656a89c5d50ecbf5
minhdvfb <92931044+minhdvfb@users.noreply.github.com>
2021-10-21 06:45:58 -0700

    Update gioithieu.php

--- gioithieu.php
+++ gioithieu.php
@@ -1,1 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	    <meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' name='viewport'/>
+<meta content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5' name='viewport'/>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+<meta content='web' name='generator'/>
+	<meta http-equiv="X-UA-Compatible" content="Trang web chia sẻ tool tds" />
+	<title>Minh Nguyễn</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	
+</head> 
+<body>
+	<style type="text/css">
+        aimg{
+            filter: drop-shadow(7px 7px 10px blue);
+            
+        }
+		.avatar {
+			max-width: 100px;
+  height: 100px;
+			
+			border: 5px solid blue;
+			border-radius: 50%;
+			}
+			    .profile-card__name {
+				
+				color: blue;
+	font-size: 30;
+	text-align:center;
+				}
+				.tichxanh {
+					height: 100px;
+					}
 
+body {
+	align-items: center;
+}
+
+
+.fb {
+			max-width: 50px;
+  height: 50px;
+			border: 1px solid blue;
+			
+			border-radius: 50%;
+			}
+			fb1{
+            filter: drop-shadow(5px 5px 5px blue);
+            
+        }
+        .thong-tin{
+        	color: blue;
+						
+						text-align:center;
+						}
+						.zalo{
+							
+			background: #456BD9;
+  border: 0.1875em solid blue;
+  border-radius: 50%;
+  box-shadow: 0.375em 0.375em 0 0 rgba(15, 28, 63, 0.125);
+  height: 50px;
+  width: 50px;
+			}
+			zalo1{
+				filter: drop-shadow(5px 5px 5px blue);
+				}
+			.atm{
+				justify-content: center;
+				text-align:center;
+				display: flex;
+				}
+				
+			</style>
+			<div class="body_view container col">
+			<div class="display-flex">
+			<div class="atm bg-dark">
+			<div class="col col-lg-12">
+                    <div class="card-body pt-2">
+			<div class="view_key">
+                      <div class="card card-mini mb-4">
+                        <div class="card-body">
+			<div class="text-center">
+<aimg><img class="avatar" src="/avt.png" ></aimg>
+</div>
+<div class='profile-card__name'>Minh Nguyễn<img class="tichxanh" src="https://i0.wp.com/s1.uphinh.org/2021/07/21/Tich-xanh.png" data-toggle="tooltip" data-placement="right" title="Đã xác minh" style="width: 30px; height: 30px;">
+</div>
+<p data-toggle="tooltip" data-placement="top" data-original-title="ATM - TOOL"><span style="border-radius: 5px; background: -webkit-linear-gradient(left, #eb0101, #eb01dd);" class="badge badge-lg badge-danger"><i class="fa fa-user"></i>Admin</span> <span style="color:#1b04f9;background: -webkit-linear-gradient(left, #fffc00, #00ffe4); border-radius: 5px;" class="badge badge-lg badge-light "></span> <span style="color:white;background: -webkit-linear-gradient(left, #05e5f9, #eb0101); border-radius: 5px;" class="badge badge-lg badge-light ">Tricker Facebook</span> <span style="color:white;background: -webkit-linear-gradient(left, green, green); border-radius: 5px;" class="badge badge-lg badge-light ">Người Kiểm Duyệt</span> <span style="color:white;background: -webkit-linear-gradient(left, #0000bf, #bf00bf); border-radius: 5px;" class="badge badge-lg badge-light ">Mạng Xã Hội</span> <span style="color:white;background: -webkit-linear-gradient(left, #bf0000, #bfbf00); border-radius: 5px;" class="badge badge-lg badge-light ">Lập Trình Viên</span> <span style="color:white;background: -webkit-linear-gradient(left, red, red); border-radius: 5px;" class="badge badge-lg badge-light ">Website</span> </p
+<div color="black" class="chu"> Tên Thật </div>
+<div style="font-size: 20px">
+<script style="margin-top: 10px;"type="text/javascript">
+farbbibliothek = new Array();
+farbbibliothek[0] = new Array("#FF0000","#FF1100","#FF2200","#FF3300","#FF4400","#FF5500","#FF6600","#FF7700","#FF8800","#FF9900","#FFaa00","#FFbb00","#FFcc00","#FFdd00","#FFee00","#FFff00","#FFee00","#FFdd00","#FFcc00","#FFbb00","#FFaa00","#FF9900","#FF8800","#FF7700","#FF6600","#FF5500","#FF4400","#FF3300","#FF2200","#FF1100");
+farbbibliothek[1] = new Array("#00FF00","#000000","#00FF00","#00FF00");
+farbbibliothek[2] = new Array("#00FF00","#FF0000","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00");
+farbbibliothek[3] = new Array("#FF0000","#FF4000","#FF8000","#FFC000","#FFFF00","#C0FF00","#80FF00","#40FF00","#00FF00","#00FF40","#00FF80","#00FFC0","#00FFFF","#00C0FF","#0080FF","#0040FF","#0000FF","#4000FF","#8000FF","#C000FF","#FF00FF","#FF00C0","#FF0080","#FF0040");
+farbbibliothek[4] = new Array("#FF0000","#EE0000","#DD0000","#CC0000","#BB0000","#AA0000","#990000","#880000","#770000","#660000","#550000","#440000","#330000","#220000","#110000","#000000","#110000","#220000","#330000","#440000","#550000","#660000","#770000","#880000","#990000","#AA0000","#BB0000","#CC0000","#DD0000","#EE0000");
+farbbibliothek[5] = new Array("#000000","#000000","#000000","#FFFFFF","#FFFFFF","#FFFFFF");
+farbbibliothek[6] = new Array("#0000FF","#FFFF00");
+farben = farbbibliothek[4];
+function farbschrift(){for(var b=0;b<Buchstabe.length;b++){document.all["a"+b].style.color=farben[b]}farbverlauf()}function string2array(b){Buchstabe=new Array();while(farben.length<b.length){farben=farben.concat(farben)}k=0;while(k<=b.length){Buchstabe[k]=b.charAt(k);k++}}function divserzeugen(){for(var b=0;b<Buchstabe.length;b++){document.write("<span id='a"+b+"' class='a"+b+"'>"+Buchstabe[b]+"</span>")}farbschrift()}var a=1;function farbverlauf(){for(var b=0;b<farben.length;b++){farben[b-1]=farben[b]}farben[farben.length-1]=farben[-1];setTimeout("farbschrift()",30)}var farbsatz=1;function farbtauscher(){farben=farbbibliothek[farbsatz];while(farben.length<text.length){farben=farben.concat(farben)}farbsatz=Math.floor(Math.random()*(farbbibliothek.length-0.0001))}setInterval("farbtauscher()",5000);
+
+text= "Nguyễn Lê Trần Minh"; //h
+string2array(text);
+divserzeugen();
+//document.write(text);
+</script></div>
+<!--cái nút ở trên by keyvip.xyz-->
+<div color="black" class="chu"> Giới Tính : NAM</div><br />
+<div color="black" class="chu"> Nơi Sinh Ra : Huế</div><br />
+<div color="black" class="chu">Năm Sinh : 2008 </div><br />
+<div color="black" class="chu">Sở Thích : Lập Trình Wep + App </div><br />
+<div color="black" class="chu">Chuyên Lập Trình Ngôn Ngữ</div><br>
+<center><div color="black" class="chu">HTML,PHP,PYTHON,CSS,JAVASCRIPT</div></center>
+<h1 color="blue" class="chu1">Thành Tích </h1><br />
+
+<span>
+<a class="d-flex justify-content-center" style="text-decoration: none;">Facebook : Hơn 17K Follow</a>
+<a class="d-flex justify-content-center" style="text-decoration: none;">Được Công Nhận Sự Uy Tín Trong Nhóm Hacker & Tricker Việt Nam</a>
+</span>
+<br />
+<br />
+<h1 class='thong-tin'>Thông Tin Liên Hệ</h1>
+<div class="text-center">
+<span>
+	<a href="https://www.facebook.com/shopmnguyen/">
+<fb1><img class="fb" src="https://sv3.anh365.com/images/2021/10/12/tai-xung.png" ></fb1></a>
+<a href="//zalo.me/0582626248/">
+<zalo1><img class="zalo" src="https://sv3.anh365.com/images/2021/10/12/tai-xung-1.png" ></zalo1></a>
+</span>
+</div>
+</div>
+</div>
+
+                        </div>
+                      </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                        
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<center><small>(c) Minh Nguyễn Coppyright All Server</small></center>
+</body>
+</html>
